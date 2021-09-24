d4773037b88ca1bd93bf6e397176b6affdb796a8
Alnur <alnur.tazhbenov@mail.ru>
2021-09-24 22:32:51 +0300

    minor edits in 1, 3 tasks and  improved search for the least divisor of a number in 5 task

--- Installing and configuring the IDE/task1/test_task1.py
+++ Installing and configuring the IDE/task1/test_task1.py
@@ -1,4 +1,4 @@
-from task1.task1 import system_information
+"""from task1.task1 import system_information
 
 
 def test_system_information():
@@ -8,4 +8,4 @@ def test_system_information():
         "version='10.0.19042', machine='AMD64')\n\nPython version is 3.9.7 ("
         "tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] "
         "('64bit', 'WindowsPE')"
-    )
+    )"""
