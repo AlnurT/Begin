70c026ec76c85221df4440ed9b721ea8be4bf41a
Alnur <alnur.tazhbenov@mail.ru>
2021-10-08 21:10:19 +0300

    Task 4 - DRY principle: using the map function

--- Installing and configuring the IDE/task4/test_task4.py
+++ Installing and configuring the IDE/task4/test_task4.py
@@ -1,13 +1,5 @@
-from task4.task4 import assembly_number, reverse_number
+from task4.task4 import reverse_number
 
 
-def test_reverse_int_number():
-    assert reverse_number("1256.874", 0) == "6521"
-
-
-def test_reverse_fractional_number():
-    assert reverse_number("1256.874", 1) == "478"
-
-
-def test_assembly_number():
-    assert assembly_number("1256.874") == 6521.478
+def test_reverse_number():
+    assert reverse_number("1256.874") == "6521.478"
