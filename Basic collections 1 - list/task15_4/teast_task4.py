b82de9f80d866d7ccb2cf52e3474f9ac3da9b9d2
Alnur <alnur.tazhbenov@mail.ru>
2021-10-16 19:24:35 +0300

    Initial Task 15.4

---
+++ Basic collections 1 - list/task15_4/teast_task4.py
@@ -0,0 +1,5 @@
+from task15_4.task4 import video_cards_selection
+
+
+def test_list_without_best_video_cards():
+    assert video_cards_selection([3070, 2060, 3090, 3070, 3090]) == [3070, 2060, 3070]
