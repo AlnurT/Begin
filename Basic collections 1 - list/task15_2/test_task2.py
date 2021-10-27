874c9297b9f8e342e937da5f04dc077e20b313c8
Alnur <alnur.tazhbenov@mail.ru>
2021-10-27 19:58:08 +0300

    Task 15.2 - tests for empty list and one-person list

--- Basic collections 1 - list/task15_2/test_task2.py
+++ Basic collections 1 - list/task15_2/test_task2.py
@@ -5,3 +5,11 @@ def test_every_second_team_member():
     assert team_selection(
         ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
     ) == ["Борис", "Гоша", "Евгений", "Захар"]
+
+
+def test_only_one_person_on_the_list():
+    assert team_selection(["Артемий"]) == []
+
+
+def test_not_people_on_the_list():
+    assert team_selection([]) == []
