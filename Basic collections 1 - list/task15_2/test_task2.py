36c5967895f27b4d2a91aefe039b8b19b3ebe76a
Alnur <alnur.tazhbenov@mail.ru>
2021-11-01 14:06:39 +0300

    Task 15.2 - using new test

--- Basic collections 1 - list/task15_2/test_task2.py
+++ Basic collections 1 - list/task15_2/test_task2.py
@@ -1,15 +1,17 @@
+import pytest
 from task15_2.task2 import team_selection
 
 
-def test_every_second_team_member():
-    assert team_selection(
-        ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
-    ) == ["Борис", "Гоша", "Евгений", "Захар"]
-
-
-def test_only_one_person_on_the_list():
-    assert team_selection(["Артемий"]) == []
-
-
-def test_not_people_on_the_list():
-    assert team_selection([]) == []
+@pytest.mark.parametrize(
+    ("all_names", "team"),
+    [
+        (
+            ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"],
+            ["Борис", "Гоша", "Евгений", "Захар"],
+        ),
+        (["Артемий"], []),
+        ([], []),
+    ],
+)
+def test_every_second_team_member(all_names, team):
+    assert team_selection(all_names) == team
