1d30dfe32b0bb5fd7e5ea549af2b5e2664efc32b
Alnur <alnur.tazhbenov@mail.ru>
2021-11-09 19:22:23 +0300

    Task 15.5 - changing the function code, simplifying the logic, introducing a common variable for tests

--- Basic collections 1 - list/task15_5/test_task5.py
+++ Basic collections 1 - list/task15_5/test_task5.py
@@ -1,41 +1,21 @@
-from task15_5.task5 import favorite_films
+from task15_5.task5 import is_film_on_list
+
+data = [
+    "Крепкий орешек",
+    "Назад в будущее",
+    "Таксист",
+    "Леон",
+    "Богемская рапсодия",
+    "Город грехов",
+    "Мементо",
+    "Отступники",
+    "Деревня",
+]
 
 
-def test_favorite_films_is_in_the_list():
-    assert (
-        favorite_films(
-            [
-                "Крепкий орешек",
-                "Назад в будущее",
-                "Таксист",
-                "Леон",
-                "Богемская рапсодия",
-                "Город грехов",
-                "Мементо",
-                "Отступники",
-                "Деревня",
-            ],
-            ["Леон", "Таксист", "Варвара", "Деревня"],
-        )
-        == ["Леон", "Таксист", "Деревня"]
-    )
+def test_is_favorite_film_in_the_list():
+    assert is_film_on_list(data, "Леон")
 
 
-def test_favorite_films_isnt_in_the_list():
-    assert (
-        favorite_films(
-            [
-                "Крепкий орешек",
-                "Назад в будущее",
-                "Таксист",
-                "Леон",
-                "Богемская рапсодия",
-                "Город грехов",
-                "Мементо",
-                "Отступники",
-                "Деревня",
-            ],
-            ["Форсаж", "Боксер", "Бэтмэн"],
-        )
-        == []
-    )
+def test_is_favorite_film_not_in_the_list():
+    assert not is_film_on_list(data, "Форсаж")
