d5e3e5266d8d65e4c5cf8c793a83bcd9e5f31a32
Alnur <alnur.tazhbenov@mail.ru>
2022-01-28 16:11:21 +0300

    Task 17.1 - Paramertization of tests

--- List comprehensions/task17_1/test_task17_1.py
+++ List comprehensions/task17_1/test_task17_1.py
@@ -1,15 +1,16 @@
+import pytest
 from task17_1.task17_1 import search_for_vowels
 
 vowels = frozenset("абеёиоуыэюя")
 
 
-def test_vowels_is_in_the_text():
-    assert search_for_vowels(vowels, "ВАй ты!") == ["а", "ы"]
-
-
-def test_vowels_isnt_in_the_text():
-    assert search_for_vowels(vowels, "Вй т!") == []
-
-
-def test_empty_text():
-    assert search_for_vowels(vowels, "") == []
+@pytest.mark.parametrize(
+    ("text", "list_of_vowels"),
+    [
+        ("ВАй ты!", ["а", "ы"]),
+        ("Вй т!", []),
+        ("", []),
+    ],
+)
+def test_vowels_is_in_the_text(text, list_of_vowels):
+    assert search_for_vowels(vowels, text) == list_of_vowels
